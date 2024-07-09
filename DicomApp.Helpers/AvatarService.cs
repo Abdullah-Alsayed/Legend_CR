e73f8e84f2a465331518807fb158eb1ad854a900
Abdullah Rafaat Alsayed <48410495+Abdullah-Alsayed@users.noreply.github.com>
2024-07-09 23:30:35 +0300

    commit

--- DicomApp.Helpers/AvatarService.cs
+++ DicomApp.Helpers/AvatarService.cs
@@ -19,33 +19,41 @@ namespace DicomApp.Helpers
 
         public async Task<IFormFile> GetAvatarAsFormFileAsync(string name, int size = 64)
         {
-            var random = new Random();
-            var backgrounds = new List<string>
+            try
             {
-                "#FBA834",
-                "#333A73",
-                "#387ADF",
-                "#50C4ED",
-                "41B06E",
-                "41B06E"
-            };
-            var background = backgrounds[random.Next(0, backgrounds.Count - 1)];
-            var url =
-                $"https://ui-avatars.com/api/?name={Uri.EscapeDataString(name)}&size={size}&rounded=true&background={background}&bold=true";
-
-            using (var response = await _httpClient.GetAsync(url))
-            {
-                response.EnsureSuccessStatusCode();
-
-                var byteArray = await response.Content.ReadAsByteArrayAsync();
-                var stream = new MemoryStream(byteArray);
-
-                var formFile = new FormFile(stream, 0, stream.Length, "avatar", "avatar.png")
+                var random = new Random();
+                var backgrounds = new List<string>
                 {
-                    Headers = new HeaderDictionary(),
-                    ContentType = "image/png"
+                    "#FBA834",
+                    "#333A73",
+                    "#387ADF",
+                    "#50C4ED",
+                    "41B06E",
+                    "41B06E"
                 };
-                return formFile;
+                var background = backgrounds[random.Next(0, backgrounds.Count - 1)];
+                var url =
+                    $"https://ui-avatars.com/api/?name={Uri.EscapeDataString(name)}&size={size}&rounded=true&background={background}&bold=true";
+
+                using (var response = await _httpClient.GetAsync(url))
+                {
+                    response.EnsureSuccessStatusCode();
+
+                    var byteArray = await response.Content.ReadAsByteArrayAsync();
+                    var stream = new MemoryStream(byteArray);
+
+                    var formFile = new FormFile(stream, 0, stream.Length, "avatar", "avatar.png")
+                    {
+                        Headers = new HeaderDictionary(),
+                        ContentType = "image/png"
+                    };
+                    return formFile;
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return null;
             }
         }
     }
